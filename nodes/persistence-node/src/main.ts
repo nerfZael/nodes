d8287795945797c66fb8039b42f3710ccac4d168
Jure Bogunovic <bogunovij@gmail.com>
2022-04-09 03:24:48 +0200

    wip

--- nodes/persistence-node/src/main.ts
+++ nodes/persistence-node/src/main.ts
@@ -2,13 +2,7 @@
 import { program } from "commander";
 import { initializeCliCommands } from "./modules/cli/cli.commands";
 import { initializeDaemonCommands } from "./modules/daemon/daemon.commands";
-import fs from "fs";
 
-const version = "./v1.0.9";
-
-if(!fs.existsSync(version)) {
-    fs.writeFileSync(version, "hello");
-}
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("custom-env").env();
 
