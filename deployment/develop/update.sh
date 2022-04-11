ada6406e12d9387810bf9d3174195aa525783b29
Jure Bogunovic <bogunovij@gmail.com>
2022-04-11 23:59:08 +0200

    wip

--- deployment/develop/update.sh
+++ deployment/develop/update.sh
@@ -5,6 +5,10 @@ data=~/data
 
 persistenceNode=polywrap/nodes/nodes/persistence-node
 ensIndexerNode=polywrap/nodes/nodes/ens-contenthash-indexer-node
+persistenceNodePort=8081
+ensIndexerNodeRinkebyPort=8082
+ensIndexerNodeRopstenPort=8083
+startingBlock=10470682
 
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
@@ -31,8 +35,8 @@ node $hosting/$persistenceNode/bin/main.js init --data $data/persistence-node --
 node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/rinkeby --network rinkeby --log
 node $hosting/$ensIndexerNode/bin/main.js init --data $data/ens-indexer-node/ropsten --network ropsten --log
 
-pm2 start $hosting/$persistenceNode/bin/main.js --name persistence-node -- daemon --data $data/persistence-node --http 8081 --log
-pm2 start $hosting/$ensIndexerNode/bin/main.js --name ens-indexer-node-rinkeby -- daemon --data $data/ens-indexer-node/rinkeby --port 8082 --log --from-block 10470682 --log
-pm2 start $hosting/$ensIndexerNode/bin/main.js --name ens-indexer-node-ropsten -- daemon --data $data/ens-indexer-node/ropsten --port 8083 --log --from-block 10470682 --log
+pm2 start $hosting/$persistenceNode/bin/main.js --name persistence-node -- daemon --data $data/persistence-node --http $persistenceNodePort --log
+pm2 start $hosting/$ensIndexerNode/bin/main.js --name ens-indexer-node-rinkeby -- daemon --data $data/ens-indexer-node/rinkeby --port $ensIndexerNodeRinkebyPort --log --from-block $startingBlock --log
+pm2 start $hosting/$ensIndexerNode/bin/main.js --name ens-indexer-node-ropsten -- daemon --data $data/ens-indexer-node/ropsten --port $ensIndexerNodeRopstenPort --log --from-block $startingBlock --log
 
 pm2 save
--- nodes/persistence-node/src/services/IpfsGatewayApi.ts
+++ nodes/persistence-node/src/services/IpfsGatewayApi.ts
@@ -174,7 +174,7 @@ export class IpfsGatewayApi {
     }));
 
     app.get("/", handleError(async (req, res) => {
-      res.send("Status: running<br>Version: v1.0.17");
+      res.send("Status: running<br>Version: v1.0.18");
     }));
 
     app.get("/status", handleError(async (req, res) => {
